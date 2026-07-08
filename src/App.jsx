9d6255e890daca48179117d186f027e3df414c24
Kamesh R <kameshramesh006@gmail.com>
2026-07-09 03:59:19 +0530

    Fix import case sensitivity in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Main from './components/Main/Main';
+import Main from './Components/Main/Main';
 import Sidebar from './Components/Sidebar/Sidebar';
 
 const App = () => {
