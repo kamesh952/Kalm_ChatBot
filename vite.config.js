face11dc0cdbe96447e2898cef59ac992b936de5
Kamesh R <kameshramesh006@gmail.com>
2026-07-09 03:46:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Kalm_ChatBot/', // 👈 This must match your GitHub repo name
+  base: '/', // 👈 This must match your GitHub repo name
   plugins: [react()],
 });
